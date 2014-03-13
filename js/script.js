9491859f677147f1095aeec4ce9133bdfb6a037c
silver-s <lafferx@gmail.com>
2014-03-13 17:06:33 +0200

    Minor modifications in design and improvements in graph box handling.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
-function newGraphBlock() {
-	var newBlock = document.createElement("div");
-	newBlock.className = "ui-widget-content graphic-block"; 
-    newBlock.innerHTML = "<p>Lorem ipsum dolor sit amet</p>"; 
-	document.body.appendChild(newBlock); 
-	$(function() {
-	$( ".graphic-block" ).draggable().resizable();
+$( document ).ready(function() {
+	makeDraggable();
 });
 
+function newGraphBlock() {
+	var newGraphBlock = document.createElement("span");
+	newGraphBlock.className = "graphBlock"; 
+	newGraphBlock.id = "testplokk";
+	newGraphBlock.innerHTML = "Lorem ipsum dolor sit amet";
+	//newGraphBlock.setAttribute("contenteditable", "true");
+	document.getElementById("workbench").appendChild(newGraphBlock); 
+	makeDraggable();
+	$( "#initial-instructions" ).remove();
 }
 
-$(function() {
-	$( ".graphic-block" ).draggable().resizable();
+function makeDraggable() {
+  $( ".graphBlock" ).draggable({ containment: "#workbench ", scroll: false, stack: ".graphBlock" }).resizable();
+}
+
+
+ $(function() {
+var tabs = $( "#workarea" ).tabs();
+tabs.find( ".ui-tabs-nav" ).sortable({
+axis: "x",
+stop: function() {
+tabs.tabs( "refresh" );
+}
 });
+});
+
+
+/*function moveGraphBlock() {
+	document.getElementById("testplokk").innerHTML="CHANGED";
+	
+}*/
+
+/*document.getElementById("testplokk").onmousedown=function(){
+	document.getElementById("testplokk").innerHTML="CHANGED";
+};
+*/
+

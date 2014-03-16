651c4f308b8a91eb865c94332936ee1c7057aaa3
silver-s <lafferx@gmail.com>
2014-03-16 17:50:17 +0200

    Tabs for multiple workbenches, handle for moving graphic blocks

--- js/script.js
+++ js/script.js
@@ -5,37 +5,53 @@ $( document ).ready(function() {
 function newGraphBlock() {
 	var newGraphBlock = document.createElement("span");
 	newGraphBlock.className = "graphBlock"; 
-	newGraphBlock.id = "testplokk";
-	newGraphBlock.innerHTML = "Lorem ipsum dolor sit amet";
-	//newGraphBlock.setAttribute("contenteditable", "true");
-	document.getElementById("workbench").appendChild(newGraphBlock); 
+	
+	var newGraphBlockHandle = document.createElement("span");
+	newGraphBlockHandle.className = "graphBlock-handle";
+	newGraphBlock.appendChild(newGraphBlockHandle);
+
+	var newGraphBlockContent = document.createElement("span");
+	newGraphBlockContent.className = "graphBlock-content";
+	newGraphBlockContent.innerHTML = "Lorem ipsum dolor sit amet";
+	newGraphBlockContent.setAttribute("contenteditable", "true");
+	newGraphBlock.appendChild(newGraphBlockContent);
+
+	document.getElementById(currentTab).appendChild(newGraphBlock);
 	makeDraggable();
 	$( "#initial-instructions" ).remove();
 }
 
 function makeDraggable() {
-  $( ".graphBlock" ).draggable({ containment: "#workbench ", scroll: false, stack: ".graphBlock" }).resizable();
+	$( ".graphBlock" ).draggable({ containment: currentTab, scroll: false, stack: ".graphBlock", handle: ".graphBlock-handle" }).resizable();
 }
 
+/* ===Tabs=== */
 
- $(function() {
-var tabs = $( "#workarea" ).tabs();
-tabs.find( ".ui-tabs-nav" ).sortable({
-axis: "x",
-stop: function() {
-tabs.tabs( "refresh" );
+var currentTab = "workbench-1";
+var tabCounter = 2;
+var tabs = $( "#workarea" ).tabs({
+	active: 1,
+	activate: function( event, ui ) {
+		var active = $("#workarea").tabs("option", "active");
+        currentTabHash = $("#workarea ul>li a").eq(active).attr('href');
+        currentTab = currentTabHash.substr(1, currentTabHash.length);
+	},
+	beforeActivate: function( event, ui ) {
+		if (ui.newPanel[0].id === "addNewTab") {
+			addNewTab();
+			return false;
+		}
+	}
+});
+
+
+function addNewTab() {
+	var id = "workbench-" + tabCounter,
+	li = "<li><a href='#" + id + "'>" + tabCounter + "</a></li>";
+	tabs.find( ".ui-tabs-nav" ).append( li );
+	tabs.append( "<div id='" + id + "' class='workbench'></div>" );
+	tabs.tabs( "refresh" );
+	tabCounter++;
 }
-});
-});
 
-
-/*function moveGraphBlock() {
-	document.getElementById("testplokk").innerHTML="CHANGED";
-	
-}*/
-
-/*document.getElementById("testplokk").onmousedown=function(){
-	document.getElementById("testplokk").innerHTML="CHANGED";
-};
-*/
-
+/* ========= */
